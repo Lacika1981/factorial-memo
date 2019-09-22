76f3a3fc8b953420c444a827ddf11db9a208511f
Lacika1981 <vargalaszlo1981@googlemail.com>
2019-09-22 23:02:31 +0100

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -5,7 +5,6 @@ function factorial() {
 			console.log('from memo');
 			return memo[n];
 		}
-        console.log(memo);
         if (n <= 1) return n;
         return memo[n] = n * innerFactorial(n - 1, memo);	
 	}
